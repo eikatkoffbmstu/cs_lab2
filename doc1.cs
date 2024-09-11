c7dad07bf3d774f8d2f0e8ca1311c18315d30699
eikatkoffbmstu <147157589+eikatkoffbmstu@users.noreply.github.com>
2024-09-11 23:24:54 +0300

    Create doc1.cs

---
+++ doc1.cs
@@ -0,0 +1,58 @@
+using System;
+class DocumentWorker
+{
+    public virtual void OpenDocument()
+    {
+        Console.WriteLine("Документ открыт");
+    }
+    public virtual void EditDocument()
+    {
+        Console.WriteLine("Редактирование документа доступно в версии Pro");
+    }
+    public virtual void SaveDocument()
+    {
+        Console.WriteLine("Сохранение документа доступно в версии Pro");
+    }
+}
+class ProDocumentWorker : DocumentWorker
+{
+    public override void EditDocument()
+    {
+        Console.WriteLine("Документ отредактирован");
+    }
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Expert");
+    }
+}
+class ExpertDocumentWorker : ProDocumentWorker
+{
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в новом формате");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentWorker documentWorker;
+        Console.WriteLine("Введите номер ключа доступа (pro/expert): ");
+        string key = Console.ReadLine();
+        switch (key.ToLower())
+        {
+            case "pro":
+                documentWorker = new ProDocumentWorker();
+                break;
+            case "expert":
+                documentWorker = new ExpertDocumentWorker();
+                break;
+            default:
+                documentWorker = new DocumentWorker();
+                break;
+        }
+        documentWorker.OpenDocument();
+        documentWorker.EditDocument();
+        documentWorker.SaveDocument();
+    }
+}
